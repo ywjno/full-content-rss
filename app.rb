45c4c49df6bb56e915a1fbc0a3110bc86d1fbef0
ywjno <ywjno.dev@gmail.com>
2012-12-06 14:53:27 +0800

    使用内联模板的方式来生成rss内容

--- app.rb
+++ app.rb
@@ -26,33 +26,41 @@ class App < Sinatra::Base
     end
   end
 
+  configure do
+    set :inline_templates, true
+  end
+
   get '/' do
     "<a href='/rss.xml'>#{settings.site_name}</a>"
   end
 
   get '/rss.xml' do
-    content_type 'application/rss+xml'
-    builder do |xml|
-      xml.instruct! :xml, :version => '1.0', :encoding => 'UTF-8'
-      xml.rss :version => "2.0" do
-        xml.channel do
-          xml.title settings.xml_title
-          xml.description settings.xml_description
-          xml.link settings.web_link
-
-          RssItem.order_by(pubDate: :desc).limit(50).each do |item|
-            xml.item do
-              xml.title item.title
-              xml.link item.link
-              xml.guid item.guid
-              xml.description item.body
-              xml.pubDate item.pubDate.getlocal(settings.time_zone).rfc822()
-            end
-          end
-        end
-      end
-    end
+    @rss_items = RssItem.order_by(pubDate: :desc).limit(50)
+    content_type 'application/rss+xml', :charset => 'utf-8'
+    builder :rss
   end
 
   run! if app_file == $0
+end
+
+__END__
+
+@@ rss
+xml.instruct! :xml, :version => '1.0', :encoding => 'UTF-8'
+xml.rss :version => "2.0" do
+  xml.channel do
+    xml.title settings.xml_title
+    xml.description settings.xml_description
+    xml.link settings.web_link
+
+    @rss_items.each do |item|
+      xml.item do
+        xml.title item.title
+        xml.link item.link
+        xml.guid item.guid
+        xml.description item.body
+        xml.pubDate item.pubDate.getlocal(settings.time_zone).rfc822()
+      end
+    end
+  end
 end
