cbf6ed6872aa1d17d65e5aa6f18af5b571639669
ywjno <ywjno.dev@gmail.com>
2013-01-14 17:05:41 +0800

    把rss内容的处理移至model类里面

--- lib/crawl_util.rb
+++ lib/crawl_util.rb
@@ -1,32 +1,23 @@
 # encoding: utf-8
 
-require 'nokogiri'
-require 'open-uri'
-
 require File.expand_path("../../models/rss_item", __FILE__)
 
 class CrawlUtil
-  RSS_URL = YAML.load_file(File.expand_path("../../config/app_configs.yml", __FILE__))['rss_url']
-
   class << self
     def analyze(status = 'init')
-      rss = Nokogiri::XML(open(RSS_URL))
       today = Time.now.to_date
       count = 0
       RssItem.delete_all if 'init' == status
       File.open(File.expand_path("../../log/info.log", __FILE__), "w") do |log_file|
         log_file.puts "#{Time.now}====>start #{status} data."
-        rss.css('item').each do |item|
-          rss_item = extract(item)
+        RssItem.extract_xml_item.each do |xml_item|
+          rss_item = RssItem.new
+          rss_item.extract_item(xml_item)
           if 'init' == status
-            rss_item.body = body(rss_item.link)
-            rss_item.save
+            rss_item.save!
           else
-            if valid(rss_item, today)
-              break
-            end
-            rss_item.body = body(rss_item.link)
-            rss_item.save
+            break if rss_item.valid(today)
+            rss_item.save!
           end
           log_file.puts "#{Time.now}====>#{rss_item.link}"
           log_file.flush
@@ -41,34 +32,5 @@ class CrawlUtil
         log_file.puts "error #{$!.class}: #{$!.message}"
       end
     end
-
-    def extract(item)
-      # You will get text from rss xml file.
-      # Example like this:
-      rss_item = RssItem.new
-      ## rss_item.title = h(item.css('title'))
-      ## rss_item.description = h(item.css('description'))
-      ## rss_item.link = h(item.css('link'))
-      ## rss_item.guid = h(item.css('guid'))
-      ## rss_item.pubDate = Time.parse(h(item.css('pubDate')))
-      rss_item
-    end
-
-    def valid(rss_item, today)
-      # You will valid rss item data in here.
-      # Example like this:
-      ## RssItem.has?(rss_item.link) || (today - rss_item.pubDate.to_date).to_i <= 7
-    end
-
-    def body(url)
-      # You will get rss content data in here.
-      # Example like this:
-      ## Nokogiri::HTML(open(url)).css('div#content').inner_html
-    end
-
-    def h(dom)
-      Nokogiri::HTML(dom.inner_text.strip).inner_text.strip
-    end
   end
-
 end
--- models/rss_item.rb
+++ models/rss_item.rb
@@ -1,5 +1,8 @@
 # encoding: utf-8
 
+require 'nokogiri'
+require 'open-uri'
+
 class RssItem
   include Mongoid::Document
   include Mongoid::Timestamps::Created
@@ -11,11 +14,40 @@ class RssItem
   # field :body, type: String
   # field :link, type: String
   # field :guid, type: String
-  # field :pubDate, type: Time
+  # field :pub_date, as: :pubDate, type: Time
 
-  # validates :title, :description, :body, :link, :pubDate, presence: true
+  # validates :title, :description, :body, :link, :pub_date, presence: true
 
-  def self.has?(link)
-    self.where(link: link).exists?
+  before_validation :extract_body
+
+  def self.extract_xml_item
+    rss_url = YAML.load_file(File.expand_path("../../config/app_configs.yml", __FILE__))['rss_url']
+    Nokogiri::XML(open(rss_url)).css('item')
+  end
+
+  def extract(xml_item)
+    # You will get text from rss xml file.
+    # Example like this:
+    ## self.title = h(xml_item.css('title'))
+    ## self.description = h(xml_item.css('description'))
+    ## self.link = h(xml_item.css('link'))
+    ## self.guid = h(xml_item.css('guid'))
+    ## self.pub_date = Time.parse(h(xml_item.css('pubDate')))
+  end
+
+  def valid(today)
+    # You will valid rss item data in here.
+    # Example like this:
+    ## RssItem.where(link: link).exists? || (today - rss_item.pubDate.to_date).to_i <= 7
+  end
+
+  def extract_body
+    # You will get rss content data in here.
+    # Example like this:
+    ## self.body = Nokogiri::HTML(open(link)).css('div#content').inner_html.strip
+  end
+
+  def h(dom)
+    Nokogiri::HTML(dom.inner_text.strip).inner_text.strip
   end
 end
